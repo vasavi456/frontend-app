f4ba0798e3daf055178f72a19dd8d83efe4e368a
vasavi456 <yennavasavireddy@gmail.com>
2026-03-12 15:27:05 +0530

    end

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import axios from "axios";
-import "./Content.css"
-const API_URL=import.meta.env.VITE_API_URL
+import "./Content.css";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 function Content() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  const { user, setUser, cart, setCart } = useContext(AppContext);
   const fetchProducts = async () => {
     const url = `${API_URL}/store`;
     const res = await axios.get(url);
@@ -19,27 +17,31 @@ function Content() {
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const addToCart = (product) => {
+    const found = cart.find((item) => item._id === product._id);
+    if (!found) {
+      product.quantity = 1;
+      setCart([...cart,product]);
+    }
+  };
+
   return (
     <div>
-     
-      {/* <button onClick={decrement}>-</button>
-      {count}
-      <button onClick={increment}>+</button>
-      <hr /> */}
       <div className="row">
         {products.map((product) => (
           <div className="box">
-            <img src={`${API_URL}/${product.imageUrl}`} width="300px" alt="" / >
+            <img src={`${API_URL}/${product.imageUrl}`} width="300px" alt="" />
             <h3>{product.name}</h3>
             <p>{product.desc}</p>
             <h4>{product.price}</h4>
-            <p><button>add to cart</button></p>
-            </div>
-
+            <p>
+              <button onClick={() => addToCart(product)}>Add to Cart</button>
+            </p>
+          </div>
         ))}
       </div>
     </div>
   );
 }
-
 export default Content;

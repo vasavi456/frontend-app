7ae102eb4f2a43a80954aa807dce0dbae0100a60
vasavi456 <yennavasavireddy@gmail.com>
2026-03-13 09:33:54 +0530

    add

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,4 +1,13 @@
-function Logout(){
-    return <h1>logout</h1>
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import App, { AppContext } from "../App";
+function Logout() {
+  const { user, setUser } = useContext(AppContext);
+  const Navigate = useNavigate();
+  useEffect(() => {
+    setUser({});
+    Navigate("/");
+  }, []);
+  return <h2>Logout Page</h2>;
 }
 export default Logout;
--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../App";
 import axios from "axios";
-
-const Orders = () => {
-
+function Orders() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const { user } = useContext(AppContext);
   const [orders, setOrders] = useState([]);
 
   const fetchOrders = async () => {
-    try {
-      const res = await axios.get("http://localhost:5000/api/orders/user-orders");
-      setOrders(res.data);
-    } catch (error) {
-      console.log(error);
-    }
+    const url = `${API_URL}/orders/${user.email}`;
+    const response = await axios.get(url, {
+      headers: { Authorization: `Bearer ${user.token}` },
+    });
+    setOrders(response.data);
   };
 
   useEffect(() => {
@@ -19,37 +19,27 @@ const Orders = () => {
   }, []);
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>Your Orders</h2>
-
-      {orders.length === 0 ? (
-        <p>No Orders Found</p>
-      ) : (
-        orders.map((order) => (
-          <div
-            key={order._id}
-            style={{
-              border: "1px solid #ccc",
-              marginBottom: "15px",
-              padding: "10px",
-            }}
-          >
-            <h4>Order ID: {order._id}</h4>
-            <p>Status: {order.status}</p>
-
-            <h5>Products:</h5>
-
-            {order.products.map((item, index) => (
-              <div key={index}>
-                <p>{item.name}</p>
-                <p>Quantity: {item.quantity}</p>
-              </div>
-            ))}
-          </div>
-        ))
-      )}
+    <div>
+      <h1>My Orders</h1>
+      <div>
+        {orders &&
+          orders.map((order) => (
+            <div key={order._id}>
+              <h3>Order Id: {order.orderDate}</h3>
+              <ol>
+                {order.items.map((item) => (
+                  <li key={item._id}>
+                    {item.name}-{item.price}-{item.quantity}-
+                    {item.price * item.quantity}
+                  </li>
+                ))}
+              </ol>
+              <h3>Order Value: {order.orderValue}</h3>
+              <hr />
+            </div>
+          ))}
+      </div>
     </div>
   );
-};
-
+}
 export default Orders;
